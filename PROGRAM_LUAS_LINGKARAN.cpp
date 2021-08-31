319a045c6e92ae9a485505cb8f4f12f60141a198
jerrykevin41 <jerrykevin41@gmail.com>
2021-08-31 09:36:42 +0700

    COMMIT KELIMA

---
+++ PROGRAM_LUAS_LINGKARAN.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float pi,jarijari,hasil;
+    pi = 3.14;
+    cout<<"PROGRAM LUAS lINGKARAN"<<endl;
+    cout<<"MASUKKAN JARI JARI :";cin>>jarijari;
+    hasil = pi * (jarijari * jarijari);
+    cout<<"HASIL :"<<hasil;
+}
