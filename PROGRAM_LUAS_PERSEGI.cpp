0e7cbd919905cb4021b09b5f8ae6fc3b46856d4a
jerrykevin41 <jerrykevin41@gmail.com>
2021-08-31 09:16:29 +0700

    THIRD COMMIT

---
+++ PROGRAM_LUAS_PERSEGI.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float sisi1,sisi2,hasil;
+    cout<<"PROGRAM LUAS PERSEGI"<<endl;
+    cout<<"MASUKKAN SISI 1 :";cin>>sisi1;
+    cout<<"MASUKKAN SISI 2 :";cin>>sisi2;
+    hasil = sisi1 * sisi2;
+    cout<<"HASIL :"<<hasil;
+}
