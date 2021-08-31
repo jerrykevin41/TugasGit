4637e244ce184f0075cc503a50c8aeae78cbcb21
jerrykevin41 <jerrykevin41@gmail.com>
2021-08-31 09:59:56 +0700

    COMMIT KEENAM

---
+++ PROGRAM_LUAS_SEGITIGA.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float alas,tinggi,hasil;
+    
+    cout<<"PROGRAM LUAS SEGITIGA"<<endl;
+    cout<<"MASUKKAN ALAS :";cin>>alas;
+	cout<<"MASUKKAN TINGGI :";cin>>tinggi;
+    hasil = (alas * tinggi) / 2;
+    cout<<"HASIL :"<<hasil;
+}
