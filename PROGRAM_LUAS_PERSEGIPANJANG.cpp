3397ca0931a3862c4c7bf9f9e2b250ec7eed455f
jerrykevin41 <jerrykevin41@gmail.com>
2021-08-31 09:21:40 +0700

    initial commit

---
+++ PROGRAM_LUAS_PERSEGIPANJANG.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float panjang,lebar,hasil;
+    cout<<"PROGRAM LUAS PERSEGI PANJANG"<<endl;
+    cout<<"MASUKKAN PANJANG :";cin>>panjang;
+    cout<<"MASUKKAN LEBAR :";cin>>lebar;
+    hasil = panjang * lebar;
+    cout<<"HASIL :"<<hasil;
+}
